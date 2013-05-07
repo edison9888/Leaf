3bcd4abc39e2f29eb3df35c5a02e2e6f6c591e1c
roger <30918949@qq.com>
2013-05-07 17:25:05 +0800

    dev: add LeafSettingsViewController

---
+++ Leaf/Controllers/LeafSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeafSettingsViewController.h
+//  Leaf
+//
+//  Created by roger on 13-5-7.
+//  Copyright (c) 2013年 Mobimtech. All rights reserved.
+//
+
+#import "LeafBaseViewController.h"
+
+@interface LeafSettingsViewController : LeafBaseViewController
+
+@end
---
+++ Leaf/Controllers/LeafSettingsViewController.m
@@ -0,0 +1,29 @@
+//
+//  LeafSettingsViewController.m
+//  Leaf
+//
+//  Created by roger on 13-5-7.
+//  Copyright (c) 2013年 Mobimtech. All rights reserved.
+//
+
+#import "LeafSettingsViewController.h"
+
+@interface LeafSettingsViewController ()
+
+@end
+
+@implementation LeafSettingsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
