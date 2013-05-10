6177aa4dc66a02c26ca23adcde628874de72925b
roger <30918949@qq.com>
2013-05-10 15:29:22 +0800

    dev: better settingcell layout

--- Leaf/Controllers/LeafSettingsViewController.m
+++ Leaf/Controllers/LeafSettingsViewController.m
@@ -43,6 +43,10 @@
 
 }
 
+- (void)cleanClicked
+{
+    
+}
 
 #pragma mark - 
 #pragma mark - ViewController Lifecycle
@@ -61,6 +65,7 @@
     CGFloat offsetY = 64.0f;
     LeafSettingCell *accountSettings = [[LeafSettingCell alloc] init];
     [accountSettings addTarget:self action:@selector(accountSettingsClicked)];
+    accountSettings.hasArrow = YES;
     [accountSettings setTitle:@"账号设置"];
     [accountSettings setImage:[UIImage imageNamed:@"weibo_more"]];
     [accountSettings setOrigin:CGPointMake(kLeafSettingCellMarginLeft, offsetY)];
@@ -74,16 +79,13 @@
     [blank setTitle:@"2G/3G为无图模式"];
     [blank setImage:[UIImage imageNamed:@"image_load_more"]];
     [blank setOrigin:CGPointMake(kLeafSettingCellMarginLeft, offsetY)];
-    [blank setUserInteractionEnabled:YES];
     [_container addSubview:blank];
-    offsetY =  CGRectGetMinY(blank.frame) + 11.0f;
-    [blank release];
-
+    
     UISwitch *sw = [[UISwitch alloc] init];
     [sw addTarget:self action:@selector(switchValueChanged:) forControlEvents:UIControlEventValueChanged];
-    [sw setFrame:CGRectMake(223.0f, offsetY, 79.0f, 27.0f)];
+    [sw setFrame:CGRectMake(223.0f, 11.0f, 79.0f, 27.0f)];
     [sw setUserInteractionEnabled:YES];
-    [_container addSubview:sw];
+    [blank addSubview:sw];
     LeafConfig *config = [LeafConfig sharedInstance];
     if (config.simple) {
         [sw setOn:YES];
@@ -91,8 +93,18 @@
     else {
         [sw setOn:NO];
     }
-
     [sw release];
+    
+    offsetY = CGRectGetMaxY(blank.frame) - 1.0f;
+    [blank release];
+    
+    LeafSettingCell *clean = [[LeafSettingCell alloc] init];
+    [clean addTarget:self action:@selector(cleanClicked)];
+    [clean setTitle:@"清理缓冲"];
+    [clean setImage:[UIImage imageNamed:@"clean_more"]];
+    [clean setOrigin:CGPointMake(kLeafSettingCellMarginLeft, offsetY)];
+    [_container addSubview:clean];
+    [clean release];
 }
 
 - (void)didReceiveMemoryWarning
