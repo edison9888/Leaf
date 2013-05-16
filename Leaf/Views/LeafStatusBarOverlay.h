6e5413b5e0892d4c082b1b8bf5995f3794440725
Geph0rce <30918949@qq.com>
2013-05-16 21:51:51 +0800

    add UIColor+MLPFlatColors and LeafStatusBarOverlay

---
+++ Leaf/Views/LeafStatusBarOverlay.h
@@ -0,0 +1,13 @@
+//
+//  LeafStatusBarOverlay.h
+//  Leaf
+//
+//  Created by roger on 13-5-16.
+//  Copyright (c) 2013年 Mobimtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeafStatusBarOverlay : UIView
+
+@end
---
+++ Leaf/Views/LeafStatusBarOverlay.m
@@ -0,0 +1,22 @@
+//
+//  LeafStatusBarOverlay.m
+//  Leaf
+//
+//  Created by roger on 13-5-16.
+//  Copyright (c) 2013年 Mobimtech. All rights reserved.
+//
+
+#import "LeafStatusBarOverlay.h"
+
+@implementation LeafStatusBarOverlay
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
