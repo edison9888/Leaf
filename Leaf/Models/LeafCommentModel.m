34c1a5d3b9968d52bd17ffb351900ccfc6a1cf7a
roger <30918949@qq.com>
2013-05-22 16:42:15 +0800

    dev: disable delegate

--- Leaf/Models/LeafCommentModel.m
+++ Leaf/Models/LeafCommentModel.m
@@ -88,9 +88,9 @@
     NSLog(@"url: %@", url.absoluteString);
     ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
     [request addRequestHeader:@"Referer" value:_referer];
-    request.delegate = self;
-    [request setDidFinishSelector:@selector(requestDidFinish:)];
-    [request setDidFailSelector:@selector(requestDidFailed:)];
+    //request.delegate = self;
+    //[request setDidFinishSelector:@selector(requestDidFinish:)];
+    //[request setDidFailSelector:@selector(requestDidFailed:)];
     [request startAsynchronous];
 }
 
