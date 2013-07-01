bd722c68a2f0d97c50635988824677e3f65c0431
Geph0rce <30918949@qq.com>
2013-07-01 17:59:14 +0800

    dev: add umeng sdk
    umeng sdk is awesome!

--- Leaf/AppDelegate.m
+++ Leaf/AppDelegate.m
@@ -11,7 +11,9 @@
 #import "LeafMenuController.h"
 #import "DDMenuController.h"
 #import "SinaWeibo.h"
+#import "MobClick.h"
 
+#define UMENG_APPKEY @"51d14e7056240b58e1010bc0"
 
 @implementation AppDelegate
 
@@ -31,8 +33,34 @@
     [super dealloc];
 }
 
+- (void)umengTrack {
+    [MobClick setCrashReportEnabled:YES]; // 如果不需要捕捉异常，注释掉此行
+    [MobClick setLogEnabled:YES];  // 打开友盟sdk调试，注意Release发布时需要注释掉此行,减少io消耗
+    [MobClick setAppVersion:XcodeAppVersion]; //参数为NSString * 类型,自定义app版本信息，如果不设置，默认从CFBundleVersion里取
+    
+    [MobClick startWithAppkey:UMENG_APPKEY reportPolicy:(ReportPolicy) REALTIME channelId:nil];
+    
+    // reportPolicy为枚举类型,可以为 REALTIME, BATCH,SENDDAILY,SENDWIFIONLY几种
+    // channelId 为NSString * 类型，channelId 为nil或@""时,默认会被被当作@"App Store"渠道
+    
+    // [MobClick checkUpdate];   //自动更新检查, 如果需要自定义更新请使用下面的方法,需要接收一个(NSDictionary *)appInfo的参数
+    // [MobClick checkUpdateWithDelegate:self selector:@selector(updateMethod:)];
+    
+    [MobClick updateOnlineConfig];  //在线参数配置
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onlineConfigCallBack:) name:UMOnlineConfigDidFinishedNotification object:nil];
+    
+}
+
+- (void)onlineConfigCallBack:(NSNotification *)note {
+    
+    NSLog(@"online config has fininshed and note = %@", note.userInfo);
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self umengTrack];
+    
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     
     LeafMainViewController *mainController = [[LeafMainViewController alloc] init];
